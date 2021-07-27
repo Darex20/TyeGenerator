a3de8ec30744e9a44229e2a2d99b49f66b6cd7d2
Darex20 <pava.dario@gmail.com>
2021-07-27 12:36:43 +0200

    Minor changes(v.0.2.0)
    Changes are described in README.md file.

---
+++ static/js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
+
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function generateTyeFile(){
+    // TODO, hardcoded input
+var output =`name: myapplication
+services:
+- name: backend
+project: backend/backend.csproj
+bindings:
+- port: 7000
+- name: frontend
+project: frontend/frontend.csproj
+replicas: 2
+bindings:
+- port: 8000
+- name: worker
+project: worker/worker.csproj
+- name: rabbit
+image: rabbitmq:3-management
+bindings:
+- port: 5672
+protocol: rabbitmq`;
+
+    download("tye.yaml", output);    
+}
