90cc4fe9057b13acba6330696e1f07b6f2c27324
Darex20 <pava.dario@gmail.com>
2021-07-27 12:59:52 +0200

    Minor styling updates

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return flask.render_template('index.html')
+
+@app.route('/about')
+def about():
+    return flask.render_template('about.html')
